f967ba2501c00b70c8aaabd5b12f4f7f1b6f7c57
Mansoor Barri <mansoor.eb.ak@gmail.com>
2026-01-05 06:40:24 +0000

    fix: use false instead of null

--- src/app/actions/is-pro.ts
+++ src/app/actions/is-pro.ts
@@ -5,14 +5,12 @@ import { db } from "~/server/db";
 
 export async function isPro() {
   const { userId } = await auth();
-  if (!userId) return null;
+  if (!userId) return false;
 
   const user = await db.user.findUnique({
     where: { clerkId: userId },
     select: { role: true },
   });
-
-  // console.log("user", user)
 
   return user?.role === "PRO";
 }
