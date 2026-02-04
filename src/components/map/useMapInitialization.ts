16ba284c4615c7a047fb40a824bd4f3b79590a87
Mansoor Barri <mansoor.eb.ak@gmail.com>
2026-02-04 20:26:08 +0000

    feat: enlarge OSM map font + increase brightness

--- src/components/map/useMapInitialization.ts
+++ src/components/map/useMapInitialization.ts
@@ -113,6 +113,8 @@ export const useMapInitialization = ({
         maxZoom: 19,
         minZoom: 0,
         className: "osm-tiles",
+        tileSize: 512,
+        zoomOffset: -1,
         ...tileLoadingOptions,
       },
     );
