31c5eb25d0ceeb1679dbf34c18fec35a93cebd0e
Mansoor Barri <mansoor.eb.ak@gmail.com>
2026-02-10 22:54:41 +0000

    perf: ignore sentry hydration errors

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -10,4 +10,19 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0,
 
   integrations: [Sentry.replayIntegration()],
+
+  beforeSend(event) {
+    // Filter out hydration errors caused by browser extensions modifying the DOM
+    // eslint-disable-next-line @typescript-eslint/dot-notation
+    if (event.extra?.["__serialized__"]?.message?.includes?.("Hydration")) return null;
+    if (event.message?.includes?.("Hydration")) return null;
+    if (
+      typeof event.metadata?.title === "string" &&
+      event.metadata.title.includes("Hydration")
+    ) {
+      return null;
+    }
+
+    return event;
+  },
 });
