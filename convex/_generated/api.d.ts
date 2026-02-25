6a0cebf89332fcd1c0b17d5a41810fc2e8f5e3f8
Mansoor Barri <mansoor.eb.ak@gmail.com>
2026-02-25 06:01:56 +0000

    fix: add convex types

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,65 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as activeTrackers from "../activeTrackers.js";
+import type * as aircraftImages from "../aircraftImages.js";
+import type * as airportCharts from "../airportCharts.js";
+import type * as contributorRewards from "../contributorRewards.js";
+import type * as crons from "../crons.js";
+import type * as flights from "../flights.js";
+import type * as lib_airlineCodes from "../lib/airlineCodes.js";
+import type * as missingImageNotifications from "../missingImageNotifications.js";
+import type * as users from "../users.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
+declare const fullApi: ApiFromModules<{
+  activeTrackers: typeof activeTrackers;
+  aircraftImages: typeof aircraftImages;
+  airportCharts: typeof airportCharts;
+  contributorRewards: typeof contributorRewards;
+  crons: typeof crons;
+  flights: typeof flights;
+  "lib/airlineCodes": typeof lib_airlineCodes;
+  missingImageNotifications: typeof missingImageNotifications;
+  users: typeof users;
+}>;
+
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
